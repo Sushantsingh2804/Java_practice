a9043c17978ea809f5bc78cb943c72fe6c54d38a
sushantsingh2804 <sushantsinghruhil@gmail.com>
2022-03-14 12:47:39 +0530

    first commit

---
+++ src/helloWorld.java
@@ -0,0 +1,16 @@
+public class helloWorld {
+    public static void main(String[] args) {
+        Integer a=10,b=20,c;
+        c = a+b;
+        System.out.println("Sum is= "+c);
+        c = a-b;
+        System.out.println("Diffrence is= "+c);
+        c = a*b;
+        System.out.println("Product is= " + c);
+        c = b/a;
+        System.out.println("Division is= " + c);
+        System.out.println(a+" Incremented= "+(++a));
+        System.out.println(b+" Decremented= "+(--b));
+
+    }
+}
